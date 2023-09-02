49a085758134e50d2b6556b323012f60b6ab18cc
qmargo23 <elen.asplund@gmail.com>
2023-09-02 08:14:34 +0300

    init

---
+++ src/main/java/IntegerListImp.java
@@ -0,0 +1,71 @@
+public class IntegerListImp implements IntegerList{
+    @Override
+    public Integer add(Integer item) {
+        return null;
+    }
+
+    @Override
+    public Integer add(int index, Integer item) {
+        return null;
+    }
+
+    @Override
+    public Integer set(int index, Integer item) {
+        return null;
+    }
+
+    @Override
+    public Integer remove(Integer item) {
+        return null;
+    }
+
+    @Override
+    public Integer remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(Integer item) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(Integer item) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Integer item) {
+        return 0;
+    }
+
+    @Override
+    public Integer get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean equals(IntegerList otherList) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Integer[] toArray() {
+        return new Integer[0];
+    }
+}
